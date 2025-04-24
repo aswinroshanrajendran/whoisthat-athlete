4855de937a33da4ea208cc319d9ab4f163ed2baa
aswinroshanrajendran <aswinroshan17@gmail.com>
2025-04-24 16:09:39 +0200

    final commit

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ app =  Flask(__name__)
 @app.route('/classify_image', methods=['GET','POST'])
 
 def classify_image():
-    image_dat = request.form['image_data']
+    image_data = request.form['image_data']
 
     response = jsonify(util.classify_image(image_data))
 
